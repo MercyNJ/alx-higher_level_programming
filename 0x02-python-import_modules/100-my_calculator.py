3f614c27039f6afe084dab1bd7064c7f6a529365
MercyNJ <mercinjuguna@gmail.com>
2023-05-11 03:57:03 -0700

    Correction to comply with PEP 8 guidelines

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,9 +6,11 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
+
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
 
     a = int(sys.argv[1])
     op = sys.argv[2]
