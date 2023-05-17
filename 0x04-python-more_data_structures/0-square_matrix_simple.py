3dd804e505777d1d06139af115e80a8dc4131f01
MercyNJ <mercinjuguna@gmail.com>
2023-05-17 02:11:53 -0700

    Computing square value of all integers of a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for row in matrix:
+        square_row = list(map(lambda x: x * x, row))
+        new_matrix.append(square_row)
+
+    return new_matrix
