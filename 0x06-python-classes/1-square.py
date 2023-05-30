049ccbbf166951560e9e31d59fedc48e89c476fa
MercyNJ <mercinjuguna@gmail.com>
2023-05-30 03:04:37 -0700

    Demonstrating Private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 0-square.py by MercyNJ
+
+"""Defining a square """
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialization of square class
+        Args: size - Size of the square
+        """
+
+        self.__size = size
