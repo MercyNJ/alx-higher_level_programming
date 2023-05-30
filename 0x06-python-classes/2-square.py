47ac1ad7c1c142fe6fb29f40e85067781285f527
MercyNJ <mercinjuguna@gmail.com>
2023-05-30 05:37:57 -0700

    A class for a square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Definining a square """
+
+
+class Square:
+    """A class square representation """
+
+    def __init__(self, size=0):
+        """Initialization of square class
+        Args:
+            size: Size of the square
+        Raises:
+            TypeError: if size is not int
+            ValueError: if size less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
