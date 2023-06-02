56c7c65d91601567d14dcad0a1bcb77e66fa32c8
MercyNJ <mercinjuguna@gmail.com>
2023-06-02 11:37:58 -0700

    Function prints Python strings

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,28 @@
+#include "Python.h"
+
+/**
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
+ */
+void print_python_string(PyObject *p)
+{
+	long int str_len;
+
+	fflush(stdout);
+
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	str_len = ((PyASCIIObject *)(p))->str_len;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  str_len: %ld\n", str_len);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &str_len));
+}
