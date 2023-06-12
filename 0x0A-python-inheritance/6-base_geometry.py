9802ef5d400ad7936293500b8b2bb0d59136dfa4
MercyNJ <mercinjuguna@gmail.com>
2023-06-12 02:56:25 -0700

    Adding public instance method to class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Contains definition of an empty cls BaseGeometry."""
+
+
+class BaseGeometry:
+    """The base geometry class representation."""
+
+    def area(self):
+        """Method not yet implemented."""
+        raise Exception("area() is not implemented")
