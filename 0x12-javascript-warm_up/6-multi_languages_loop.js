88dc6d69ff2dae6b873175220475123d1c21f821
MercyNJ <mercinjuguna@gmail.com>
2023-08-14 05:17:27 -0700

    A script utilizing array of string and for loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// A script that  prints 3 lines by using an array of string and a loop.
+
+let i;
+const strarray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (i = 0; i < 3; i++) {
+  console.log(strarray[i]);
+}
