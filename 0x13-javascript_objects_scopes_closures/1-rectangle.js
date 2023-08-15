15d35e92e2530c5642780815ca960baf9b594378
MercyNJ <mercinjuguna@gmail.com>
2023-08-15 02:33:18 -0700

    Class rectangle with instance attributes added

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// A script that contains a class Rectangle that defines a rectangle.
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
