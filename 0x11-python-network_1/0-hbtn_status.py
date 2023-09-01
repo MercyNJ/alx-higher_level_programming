f162d933eab60b0a585ad5f1d7d4b754c4afa3d8
MercyNJ <mercinjuguna@gmail.com>
2023-09-01 04:57:50 -0700

    using urlib to make http request

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A script that  fetches https://alx-intranet.hbtn.io/status using urllib. """
+
+import urllib.request
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", repr(body))
+        print("\t- utf8 content:", body.decode("utf-8"))
